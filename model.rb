896e653fc40bf41349ef926c0e476d76da1058a0
Michelle9 <jhoanmary@gmail.com>
2014-11-07 03:54:51 +0000

    nuevo model.rb

--- model.rb
+++ model.rb
@@ -1,9 +1,29 @@
-class ShortenedUrl
+class Shortenedurl
   include DataMapper::Resource
 
   property :id, Serial
   property :url, Text
-  property :to, Text
+  property :to, Text 
   property :id_usu, Text 
+  property :num_visit, Integer
+  
+  has n, :visits  
+
+end
+
+class Visit
+  include DataMapper::Resource
+
+  property  :id,          Serial
+  property  :created_at,  DateTime
+  property  :ip,          IPAddress
+  property  :country,     String
+  property :countryCode, String
+  property :city, String
+  property :latitud, String
+  property :longitud, String
+  
+  belongs_to  :shortenedurl
+
 end
 
